09328b2372b714c2c28d5b01fb15848e928d8776
YousefMorad <yosef.murad@gmail.com>
2021-02-01 13:06:09 +0800

    First Deployment

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = function Navbar() {
+    return (
+        <nav className="navbar navbar-dark bg-dark mb-5">
+        <span className="navbar-brand mb-0 h1 mx-auto">lyricsz</span>  
+
+            
+        </nav>
+    );
+};
+
+export default Navbar;
